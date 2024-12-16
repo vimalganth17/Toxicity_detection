e47007d7ed98b7d9acc2775d3d1cf8ed7f5903d1
Vimal Ganth <vimalganth17@gmail.com>
2024-12-16 12:58:11 +0530

    dome

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 const ENV = {
     perspectiveApiKey: "AIzaSyBVn5rsPqKyZ5wXq-vRlSKLj3dhqEX-ES0",
     youtubeApiKey: "AIzaSyA-sAkQt0lopfTSs9DrdbNdM55qDoNqclk",
+    groqApiKey:"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"
   };
   
